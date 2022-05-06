f3accc9adda04816264e5f9595ded7f3799bd955
KuznetsovVN <kuznetsov.vladimir.n@gmail.com>
2022-05-06 12:57:34 +0300

    Added non-minified mode of building

--- build-dev.js
+++ build-dev.js
@@ -13,7 +13,8 @@ config.optimization.splitChunks = {
 config.optimization.runtimeChunk = false;
 
 // JS
-config.output.filename = '[name].js';
+config.output.filename = 'static/js/[name].js';
+
 // CSS. "5" is MiniCssPlugin
-config.plugins[5].options.filename = '[name].css';
+config.plugins[5].options.filename = 'static/css/[name].css';
 config.plugins[5].options.publicPath = '../';
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
+const path = require("path");
 var fs = require('fs');
+var beautify = require('js-beautify').js;
 var gulp = require('gulp');
 
 gulp.task('default', function() {
@@ -6,15 +8,29 @@ gulp.task('default', function() {
 });
 
 gulp.task('build-crm-module', function(done) {
-  clearBuildPluginFolder();
-  var itemsToReplace = copyNecessaryFiles();
-  replaceLinksInMainPage(itemsToReplace);
+  buildCrmModule();
+  done();
+});
+
+gulp.task('build-crm-module-dev', function(done) {
+  buildCrmModule();
+
+  const jsFile = path.resolve(__dirname, './' + TARGET_DIR + '/' + TARGET_NAME + '.js');
+  var data = fs.readFileSync(jsFile, {encoding:'utf8', flag:'r'});
+  var new_data = beautify(data, { indent_size: 2, space_in_empty_paren: true });
+  fs.writeFileSync(jsFile, new_data, { encoding: "utf8" });
 
   done();
 });
 
 const TARGET_DIR = 'build-crm-module';
 const TARGET_NAME = 'crm-datagrid';
+
+function buildCrmModule() {
+  clearBuildPluginFolder();
+  var itemsToReplace = copyNecessaryFiles();
+  replaceLinksInMainPage(itemsToReplace);
+}
 
 function clearBuildPluginFolder() {
   var dir = './' + TARGET_DIR;
@@ -31,47 +47,36 @@ function copyNecessaryFiles() {
 
   fs.copyFileSync('build/index.html', TARGET_DIR + '/' + TARGET_NAME + '.html');
 
-  fs.readdir('build/static/js', (err, files) => {
-    files.forEach(file => {
-      if(file.startsWith('main') && file.endsWith('.js')) {
-        fs.copyFileSync('build/static/js/' + file, TARGET_DIR + '/' + TARGET_NAME + '.js');
-        itemsToReplace.push(['/static/js/' + file, TARGET_NAME + '.js']);
-      }
-    });
+  const jsFiles = fs.readdirSync('build/static/js');
+  jsFiles.forEach(file => {
+    if(file.startsWith('main') && file.endsWith('.js')) {
+      fs.copyFileSync('build/static/js/' + file, TARGET_DIR + '/' + TARGET_NAME + '.js');
+      itemsToReplace.push(['/static/js/' + file, TARGET_NAME + '.js']);
+    }
   });
 
-  fs.readdir('build/static/css', (err, files) => {
-    files.forEach(file => {
-      if(file.startsWith('main') && file.endsWith('.css')) {
-        fs.copyFileSync('build/static/css/' + file, TARGET_DIR + '/' + TARGET_NAME + '.css');
-        itemsToReplace.push(['/static/css/' + file, TARGET_NAME + '.css']);
-      }
-    });
+  const cssFiles = fs.readdirSync('build/static/css');
+  cssFiles.forEach(file => {
+    if(file.startsWith('main') && file.endsWith('.css')) {
+      fs.copyFileSync('build/static/css/' + file, TARGET_DIR + '/' + TARGET_NAME + '.css');
+      itemsToReplace.push(['/static/css/' + file, TARGET_NAME + '.css']);
+    }
   });
 
   return itemsToReplace;
 }
 
 function replaceLinksInMainPage(itemsToReplace) {
-  fs.readFile(TARGET_DIR + '/' + TARGET_NAME + '.html', 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-
-    var result = data;
-    itemsToReplace.forEach(item => {
-      result = result.replace(item[0], item[1]);
-    });
-
-    var externalLinks = '<script crossorigin src="https://unpkg.com/react@17/umd/react.production.min.js"></script>' +
-                        '<script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>' +
-                        '<script crossorigin src="https://unpkg.com/@fluentui/react@8/dist/fluentui-react.js"></script>' +
-                        '<script crossorigin src="https://unpkg.com/@fluentui/react-hooks@8/dist/react-hooks.js"></script>';
-
-    result = result.replace('</head>', externalLinks + '</head>');
-
-    fs.writeFile(TARGET_DIR + '/' + TARGET_NAME + '.html', result, 'utf8', function (err) {
-      if (err) return console.log(err);
-    });
+  var result = fs.readFileSync(TARGET_DIR + '/' + TARGET_NAME + '.html', {encoding:'utf8', flag:'r'});
+  itemsToReplace.forEach(item => {
+    result = result.replace(item[0], item[1]);
   });
+
+  var externalLinks = '<script crossorigin src="https://unpkg.com/react@17/umd/react.production.min.js"></script>' +
+                      '<script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>' +
+                      '<script crossorigin src="https://unpkg.com/@fluentui/react@8/dist/fluentui-react.js"></script>' +
+                      '<script crossorigin src="https://unpkg.com/@fluentui/react-hooks@8/dist/react-hooks.js"></script>';
+
+  result = result.replace('</head>', externalLinks + '</head>');
+  fs.writeFileSync(TARGET_DIR + '/' + TARGET_NAME + '.html', result, { encoding: "utf8" });
 }
